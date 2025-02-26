46c4011212ce0c2f8de908d20bcc35f5588e6fa8
hakki17 <mariapaulasm2018@gmail.com>
2025-02-25 22:01:58 -0500

    melocaramelo

---
+++ src/main/java/edu/eci/cvds/ParcialOne/ParcialOneApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds.ParcialOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/cvds/ParcialOne/ParcialOneApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds.ParcialOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
